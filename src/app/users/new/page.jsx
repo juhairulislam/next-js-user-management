c105a9d60272f1139be91bb5a647e2d233d39258
juhairulislam <juhairulislam2018@gmail.com>
2026-05-05 21:08:00 +0600

    design form of hero UI

--- src/app/users/new/page.jsx
+++ src/app/users/new/page.jsx
@@ -1,9 +1,71 @@
+"use client";
 import React from 'react';
+import {FloppyDisk} from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  FieldGroup,
+  Fieldset,
+  Form,
+  Input,
+  Label,
+  TextArea,
+  TextField,
+} from "@heroui/react";
 
 const NewUserPage = () => {
+
+    const onSubmit = (e) =>{
+        e.preventDefault() ;
+    }
+
     return (
-        <div>
-            <h2>Create a new user</h2>
+        <div className='p-15'>
+            <h2 className='text-center my-6 text-4xl font-bold'>Create a new user</h2>
+
+            <div className='flex justify-center items-center gap-6'>
+
+            <Form className="w-full max-w-96" onSubmit={onSubmit}>
+      <Fieldset>
+        <Fieldset.Legend>Profile Settings</Fieldset.Legend>
+        <Description>Update your profile information.</Description>
+        <FieldGroup>
+          <TextField
+            isRequired
+            name="name"
+            validate={(value) => {
+              if (value.length < 3) {
+                return "Name must be at least 3 characters";
+              }
+              return null;
+            }}
+          >
+            <Label>Name</Label>
+            <Input placeholder="John Doe" />
+            <FieldError />
+          </TextField>
+          <TextField isRequired name="email" type="email">
+            <Label>Email</Label>
+            <Input placeholder="john@example.com" />
+            <FieldError />
+          </TextField>
+          
+        </FieldGroup>
+        <Fieldset.Actions>
+          <Button type="submit">
+            <FloppyDisk />
+            Save changes
+          </Button>
+          <Button type="reset" variant="secondary">
+            Cancel
+          </Button>
+        </Fieldset.Actions>
+      </Fieldset>
+    </Form>
+
+            </div>
+
         </div>
     );
 };
