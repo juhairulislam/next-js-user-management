352f8056f21174467f4e89d3e820716a851cc09b
juhairulislam <juhairulislam2018@gmail.com>
2026-05-05 21:01:14 +0600

    create new route in users new

---
+++ src/app/users/new/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewUserPage = () => {
+    return (
+        <div>
+            <h2>Create a new user</h2>
+        </div>
+    );
+};
+
+export default NewUserPage;
