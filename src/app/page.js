6a6a1848ac5d6b203ca7514d4049906ed3b31a57
juhairulislam <juhairulislam2018@gmail.com>
2026-05-05 17:57:33 +0600

    fetch data and create user card

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
+
+      hi , this is home page 
      
     </div>
   );
---
+++ src/app/users/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const UsersPage =async  () => {
+
+    const res =await fetch('http://localhost:8000/users') ;
+    const users = await res.json() ;
+
+    console.log(users) ;
+
+    return (
+        <div className='p-10'>
+            <h2>Users: {users.length}</h2>
+
+            <div className='grid grid-cols-3 gap-6'>
+                {
+                    users.map(user => <div className='border rounded-xl shadow hover:shadow-xl transition duration-200 p-4' key={user.id}>
+                        <p className='font-bold text-xl'>{user.name}</p>
+                        <p className='font-bold '>{user.age}</p>
+                        <p className='font-bold '>{user.email}</p>
+                    </div>)
+                }
+            </div>
+            
+        </div>
+    );
+};
+
+export default UsersPage;
